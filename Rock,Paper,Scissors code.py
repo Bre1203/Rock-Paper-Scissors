8c333eff5a865c742a67a68ef664c029e2546920
Breanne Robinson <106432763+Bre1203@users.noreply.github.com>
2022-08-12 01:10:33 -0700

    Update Rock,Paper,Scissors code.py

--- Rock,Paper,Scissors code.py
+++ Rock,Paper,Scissors code.py
@@ -1,33 +1,28 @@
 import random
 
 def rock_paper_scissors():
-    print("Rock,Paper,Scissors,1,2,3!")
-    player = input("Pick one! r,p,or s: ")
-    ai_player = random.randint(1, 3)
-    if player != 'r,s,or p':
-        player=input("Oops...you didn't pick r,p,or s: ")
+    print('rock,paper,scissors, 1, 2, 3,!')
+    player=input('Pick one! (r,p,s!): ')
+    ai_player=random.randint(1,3)
+    if ai_player ==1:
+        ai_player= 'r'
 
-
-    if ai_player == 1:
-        ai_player = 'r'
-
-    if ai_player == 2:
+    if ai_player ==2:
         ai_player = 'p'
 
-    if ai_player == 3:
-        ai_player = 's'
+    if ai_player==3:
+        ai_player='S'
 
-    print("Computer played: " + ai_player)
-    print("You played: " + player)
+    print('Computer played:  '+ ai_player)
+    print('You played: '+ player)
 
     if ai_player == player:
-        print("Draw!")
-
-    elif(ai_player == 'r' and player == 's') or (ai_player == 'p' and player == 'r')\
-    or (ai_player == 's' and player == 'p'):
-        print("You Loss")
+        print('draw!')
+    elif (ai_player=='r' and player =='s') or (ai_player=='p' and player =='r') \
+    or (ai_player=='s' and player =='p'):
+        print('loss!')
 
     else:
-        print("You Won!")
+       print('Win!')
 
 rock_paper_scissors()
